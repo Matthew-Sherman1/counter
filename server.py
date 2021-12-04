1ed91feed767df81b912567e71652124d1d196d4
Matthew Sherman <matt5064@yahoo.com>
2021-12-03 23:42:30 -0700

    counter final

---
+++ server.py
@@ -0,0 +1,21 @@
+
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "counter keyyyyyyyy"
+@app.route('/')       
+def index():
+    if 'count' in session:
+        session['count'] += 1
+    else:
+        session['count'] = 1
+    return render_template("index.html")
+@app.route('/count')
+def addToCounter():
+    # session['count'] += 1 
+    return redirect('/')
+@app.route('/reset')
+def reset():
+    session['count'] = 0
+    return redirect('/')
+if __name__=="__main__":   
+    app.run(debug=True) 
